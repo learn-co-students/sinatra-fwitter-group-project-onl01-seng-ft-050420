32dd05618864559f3fdfc1dc6939cef766a12877
Luke Chen <luke.chen.2010@gmail.com>
2020-06-07 18:11:58 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,31 @@
 class TweetsController < ApplicationController
   
   get '/tweets' do
+    @tweets = Tweet.all
+    
     if logged_in?
       erb :'tweets/tweets'
     else
       redirect '/login'
+    end
+  end
+  
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+  
+  post '/tweets' do
+    content = params[:content]
+    id = current_user.id
+    
+    if content != ""
+      @tweet = Tweet.create(content: content, user_id: id)
+    else
+      redirect '/tweets/new'
     end
   end
 

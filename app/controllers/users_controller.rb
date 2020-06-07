cd70b19a567361dd22a40e8ad2a9dbd231b55803
Luke Chen <luke.chen.2010@gmail.com>
2020-06-07 05:32:21 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+    else
+      erb :'/users/create_user'
+    end
+  end
+  
+  post '/signup' do
+    username = params[:username]
+    email = params[:email]
+    password = params[:password]
+     
+    if username == "" || email == "" || password == ""
+      redirect '/signup'
+    else
+      @user = User.create(username: username, email: email, password: password)
+      redirect '/tweets'
+    end
+  
+  end
 
 end

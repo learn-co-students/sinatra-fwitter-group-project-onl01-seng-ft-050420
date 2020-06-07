d6b98d2ff9ffd6bbfe4622a9a83341b27f9d7b35
Luke Chen <luke.chen.2010@gmail.com>
2020-06-07 15:02:45 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if logged_in?
+      redirect to '/tweets'
     else
-      erb :'/users/create_user'
+      erb :'users/create_user'
     end
   end
   
@@ -18,7 +19,13 @@ class UsersController < ApplicationController
       @user = User.create(username: username, email: email, password: password)
       redirect '/tweets'
     end
-  
   end
-
+  
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'users/login'
+    end
+  end
 end

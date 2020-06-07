af99a3c6f8a26c4b923b382c0e6b1156c40779ae
Luke Chen <luke.chen.2010@gmail.com>
2020-06-07 15:11:01 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
       redirect '/signup'
     else
       @user = User.create(username: username, email: email, password: password)
+      session[:user_id] = @user.id
       redirect '/tweets'
     end
   end
@@ -26,6 +27,27 @@ class UsersController < ApplicationController
       redirect '/tweets'
     else
       erb :'users/login'
+    end
+  end
+  
+  post '/login' do
+    username = params[:username]
+    user = User.find_by(username: username)
+    
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
     end
   end
 end

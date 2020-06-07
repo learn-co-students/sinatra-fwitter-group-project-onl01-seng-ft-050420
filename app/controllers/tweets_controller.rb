01a2a81a723d228cbe24b6837f646fea7f1d8cba
Luke Chen <luke.chen.2010@gmail.com>
2020-06-07 18:43:46 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,5 +28,65 @@ class TweetsController < ApplicationController
       redirect '/tweets/new'
     end
   end
-
+  
+  get '/tweets/:id' do
+    id = params[:id]
+    
+    if logged_in?
+      @tweet = Tweet.find_by_id(id)
+      erb :'/tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+  
+  get '/tweets/:id/edit' do
+    id = params[:id]
+    user = current_user
+    
+    if logged_in?
+      @tweet = Tweet.find_by(id: id, user_id: current_user.id)
+      if @tweet != nil
+        erb :'tweets/edit_tweet'
+      else
+        redirect '/tweets'
+      end
+    else
+      redirect '/login'
+    end
+  end
+  
+  patch '/tweets/:id' do
+    id = params[:id]
+    content = params[:content]
+    user = current_user
+    
+    if logged_in?
+      @tweet = Tweet.find_by(id: id, user_id: current_user.id)
+      if @tweet != nil && content != ""
+        @tweet.content = content
+        @tweet.save
+      else
+        redirect "/tweets/#{id}/edit"
+      end
+    else
+      redirect '/login'
+    end
+  end
+  
+  delete '/tweets/:id' do
+    id = params[:id]
+    user = current_user
+    
+    if logged_in?
+      @tweet = Tweet.find_by(id: id, user_id: current_user.id)
+      if @tweet != nil
+        @tweet.delete
+      else
+        redirect "/tweets/#{id}/edit"
+      end
+    else
+      redirect '/login'
+    end
+  end
 end

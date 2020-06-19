3dcf45eeb994e1c16eca012a11117baebac286b5
Aaron Schuyler <63804815+aaron-schuyler@users.noreply.github.com>
2020-06-19 18:03:02 -0400

    Revert "Fresh Batch"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :username, presence: true
-  validates :email, presence: true
-  validates :username, uniqueness: true
   has_many :tweets
-  def slug
-    slug = self.username.split(" ")
-    slug.join("-")
-  end
 end

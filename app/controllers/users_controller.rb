355006c6c85beeaca5d362e4a63805ee54015a96
aaron-schuyler <asky28@gmail.com>
2020-06-19 15:20:06 -0400

    users can create accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
-
-
+  get '/signup' do
+    if !session[:user_id]
+      erb :'users/new'
+    else
+      redirect to '/tweets'
+    end
+  end
+  post '/signup' do
+    user = User.new(params)
+    if user.save
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
 end

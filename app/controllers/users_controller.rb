8f403ef7c4c012d8837107ce9e190f64db233936
leobr <leobrionesherrera@gmail.com>
2020-06-19 14:52:52 -0500

    Users can login and log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,38 @@ class UsersController < ApplicationController
       redirect '/signup'
     end
   end
+
+  get '/login' do
+    if !session[:user_id]
+      erb :'users/login'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/login' do 
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      @error = "wrong username or password"
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do 
+    if !session[:user_id]
+      redirect '/login'
+    else
+      session.clear
+      redirect to '/login'
+    end
+    
+  end
+
+  post '/logout' do 
+    session.clear
+    redirect to '/login'
+  end
 end
